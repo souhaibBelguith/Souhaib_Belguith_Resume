6d0a1bae677bc99907179ee3ea549f62e6eb715c
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-10-14 19:14:33 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+app.set('port', (process.env.PORT || 5000))
+
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send("index.html")
+})

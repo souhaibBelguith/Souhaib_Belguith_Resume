506bc0e214f90568d5c6ce7ab8bad7e81d658c84
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-10-15 14:05:37 +0100

    message2

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const request = require('request')
 const app=express()
 app.use(express.static(__dirname + '../public'));
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5022))
 
-
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))

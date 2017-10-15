676496bd2a2a768ba92216129adf91f7b804320c
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-10-15 13:42:16 +0100

    message2

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-app=express()
+const app=express()
 
 app.set('port', (process.env.PORT || 5000))
 
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))

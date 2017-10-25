c8bee5627b23ae320f5d3afa57c3c32765f6b355
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-10-25 07:50:26 +0100

    message2

--- php/contact1.php
+++ php/contact1.php
@@ -2,29 +2,40 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
 
-    $name = $_POST['name']; // required
-    $mail_from = $_POST['mail']; // required
-    $message = $_POST['message']; // required
-
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "email@gmail.com";
-$mail->Password = "password";
-$mail->SetFrom("sou.belguith@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = $_POST['message'];
-//$mail->AddAddress("sou.belguith@gmail.com");
-$mail->AddAddress = $_POST['mail'];
-
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
+ 
+ 
+    $sender_name = $_POST['name']; // required
+    $$sender = $_POST['mail']; // required
+    $$Body = $_POST['message']; // required
+	$Subject = "Resume mail";
+	
+  $mail = new PHPMailer;
+    //Enable SMTP debugging. 
+    $mail->SMTPDebug = 1;
+    //Set PHPMailer to use SMTP.
+    $mail->isSMTP();
+    //Set SMTP host name                          
+    $mail->Host = $hostname;
+    //Set this to true if SMTP host requires authentication to send email
+    $mail->SMTPAuth = true;
+    //Provide username and password     
+    $mail->Username = $sender;
+    $mail->Password = $mail_password;
+    //If SMTP requires TLS encryption then set it
+    $mail->SMTPSecure = "ssl";
+    //Set TCP port to connect to 
+    $mail->Port = 465;
+    $mail->From = $sender;  
+    $mail->FromName = $sender_name;
+    $mail->addAddress($to);
+    $mail->isHTML(true);
+    $mail->Subject = $Subject;
+    $mail->Body = $Body;
+    $mail->AltBody = "This is the plain text version of the email content";
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else {
+           echo 'Mail Sent Successfully';
+    }	
 ?>
